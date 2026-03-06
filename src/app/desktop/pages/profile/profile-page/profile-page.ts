58ea47d68642453fc13b7227343a2ac1d012d360
Timur Khen <timurkhen10@gmail.com>
2026-03-06 21:40:32 +0300

    profile changes

--- src/app/desktop/pages/profile/profile-page/profile-page.ts
+++ src/app/desktop/pages/profile/profile-page/profile-page.ts
@@ -1,10 +1,14 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { TuiButton } from '@taiga-ui/core';
 import { UserApiService } from '../../../../services/api/user-api/user-api-service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { StatusBlockInterface } from '../../../../interfaces/status-block/status-block-interface';
+import { debounceTime, of, Subject, switchMap, timer } from 'rxjs';
+import { StatusBlock } from "../../../components/status-block/status-block";
 
 @Component({
   selector: 'app-profile-page',
-  imports: [TuiButton],
+  imports: [TuiButton, StatusBlock],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.scss',
 })
@@ -12,6 +16,48 @@ export class ProfilePage implements OnInit {
   private userHandler = inject(UserApiService)
   userAvatarUrl = signal<string>('')
   userName = signal<string>('')
+  notificationsSubject = new Subject()
+
+  editUserForm = new FormGroup({
+    name: new FormControl(this.userName(), [Validators.required]),
+    surname: new FormControl('', [Validators.required]),
+    notifications: new FormControl()
+  })
+
+  constructor() {
+    effect(() => {
+      this.notificationsSubject
+        .pipe(
+          debounceTime(500),
+          switchMap((val: any) => {
+            return val
+          })
+        )
+        .subscribe((data) => {  
+          console.log(data)
+        }
+      )
+    })
+  }
+  
+  notificationBlock = signal<StatusBlockInterface>({
+    icon: 'bell',
+    text: 'Уведомления включены',
+    secondText: 'Уведомления отключены',
+    buttonText: 'Отключить',
+    secondButtonText: 'Включить',
+    isActive: true,
+    subject: this.notificationsSubject,
+  })
+
+  changeStatus(changingSignal: WritableSignal<StatusBlockInterface>) {
+    let value = !changingSignal().isActive
+    changingSignal.update(currentData => ({
+      ...currentData, 
+      isActive: value
+    }))
+    changingSignal().subject.next(of([value]))
+  }
 
   ngOnInit(): void {
     const userData = this.userHandler.userInfo
