5711a71c07a35d3ff1d57efcd8aca183919d7418
Timur Khen <timurkhen10@gmail.com>
2026-03-25 18:50:59 +0300

    capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'gzg-money',
+  webDir: 'dist/gzg-money/browser'
+};
+
+export default config;
